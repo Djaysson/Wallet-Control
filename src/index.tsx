d30700bbca31235f86526346de03cbc0b87bd05d
Djaysson <djayson.dev@gmail.com>
2022-04-08 20:37:48 -0300

    Listing the data coming from the API

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,28 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de website",
+          type: "deposit",
+          category: "dev",
+          amount: 7000,
+          createdAt: new Date("2022-04-08 19:45:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Ponto comercial",
+          amount: 2000,
+          createdAt: new Date("2022-04-01 08:35:00"),
+        },
+      ],
+    });
+  },
   routes() {
     this.namespace = "api";
 
