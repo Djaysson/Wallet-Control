d2314a7d490a5ee2d5aa1224810ab6b1520de753
Djaysson <djayson.dev@gmail.com>
2022-04-15 21:42:18 -0300

    Updating file imports

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
-import { FormEvent, useContext, useState } from "react";
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
-import { TransactionsContext } from "../../TransactionsContext";
+import { useTransactions } from "../../hooks/useTransactions";
+
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
 interface NewTransitionModalProps {
@@ -15,7 +16,7 @@ export function NewTransitionModal({
   isOpen,
   onRequestClose,
 }: NewTransitionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState(0);
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,10 +1,9 @@
-import { useContext } from "react";
+import { useTransactions } from "../../hooks/useTransactions";
 
-import { TransactionsContext } from "../../TransactionsContext";
 import { Container } from "./styles";
 
 export default function TransactionsTable() {
-  const { transactions } = useContext(TransactionsContext);
+  const { transactions } = useTransactions();
 
   return (
     <Container>
